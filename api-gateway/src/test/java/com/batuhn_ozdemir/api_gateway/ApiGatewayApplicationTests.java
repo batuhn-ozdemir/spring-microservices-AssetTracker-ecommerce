46e4a64d5f6f7385b2a74e3e73d8abf988b8dd67
batuhn-ozdemir <batuhn1ozdemir@gmail.com>
2025-12-30 00:18:04 +0300

    Project files added

---
+++ api-gateway/src/test/java/com/batuhn_ozdemir/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuhn_ozdemir.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ procurement-service/src/main/java/com/batuhn_ozdemir/procurement_service/ProcurementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.batuhn_ozdemir.procurement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementServiceApplication.class, args);
+	}
+
+}
