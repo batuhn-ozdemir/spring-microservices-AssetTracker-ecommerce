ae2fe5a68fff778b41a1257e7a5a420e4f279b5a
batuhn-ozdemir <batuhn1ozdemir@gmail.com>
2025-12-30 00:18:04 +0300

    Project files added

---
+++ procurement-service/src/test/java/com/batuhn_ozdemir/procurement_service/ProcurementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuhn_ozdemir.procurement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
